551477f76a258e3672d76f83981a589fb5c28f16
Bae Jin Woo <jw969696@naver.com>
2019-11-08 17:04:32 +0900

    Add files via upload

--- 3355/HowtoUse_step1.py
+++ 3355/HowtoUse_step1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'HowtoUse_step1.ui'
+# Form implementation generated from reading sui file 'HowtoUse_step1.ui'
 #
 # Created by: PyQt5 UI code generator 5.9.2
 #
@@ -8,52 +8,53 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_selectExercise(object):
-    def setupUi(self, selectExercise):
-        selectExercise.setObjectName("selectExercise")
-        selectExercise.resize(850, 600)
+class Ui_HowtoUse_step1(object):
+    def setupUi(self, HowtoUse_step1):
+        HowtoUse_step1.setObjectName("selectExercise")
+        HowtoUse_step1.resize(850, 600)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/icon_heart/heart.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        selectExercise.setWindowIcon(icon)
-        selectExercise.setStyleSheet("background-color:\"Aliceblue\";")
-        self.selectTitle = QtWidgets.QLabel(selectExercise)
-        self.selectTitle.setGeometry(QtCore.QRect(110, 450, 650, 61))
+        HowtoUse_step1.setWindowIcon(icon)
+        HowtoUse_step1.setStyleSheet("background-color:\"Aliceblue\";")
+        self.selectTitle = QtWidgets.QLabel(HowtoUse_step1)
+        self.selectTitle.setGeometry(QtCore.QRect(110, 450, 650, 60))
         self.selectTitle.setStyleSheet("font: 20pt\"경기천년제목M Medium\"; color:\"black\";")
         self.selectTitle.setObjectName("selectTitle")
-        self.Photo = QtWidgets.QWidget(selectExercise)
-        self.Photo.setGeometry(QtCore.QRect(210, 120, 431, 301))
+        self.Photo = QtWidgets.QWidget(HowtoUse_step1)
+        self.Photo.setGeometry(QtCore.QRect(210, 120, 430, 300))
         self.Photo.setAutoFillBackground(False)
-        self.Photo.setStyleSheet("background-color:\"Dodgerblue\";image: url(:/exercise1/ex1.png);")
+        self.Photo.setStyleSheet("background-color:\"red\";image: url(:/exercise1/ex1.png);")
         self.Photo.setObjectName("Photo")
-        self.exButton2 = QtWidgets.QPushButton(selectExercise)
-        self.exButton2.setGeometry(QtCore.QRect(450, 510, 131, 40))
-        self.exButton2.setStyleSheet("background-color:\"Dodgerblue\"; color:\"white\";font: 16pt\"경기천년제목M Medium\";")
-        self.exButton2.setObjectName("exButton2")
-        self.widget = QtWidgets.QWidget(selectExercise)
-        self.widget.setGeometry(QtCore.QRect(730, 20, 91, 61))
-        self.widget.setStyleSheet("background-color:\"Dodgerblue\";")
-        self.widget.setObjectName("widget")
-        self.widget_2 = QtWidgets.QWidget(selectExercise)
-        self.widget_2.setGeometry(QtCore.QRect(30, 20, 91, 61))
-        self.widget_2.setStyleSheet("background-color:\"Dodgerblue\";")
-        self.widget_2.setObjectName("widget_2")
+        self.next = QtWidgets.QPushButton(HowtoUse_step1)
+        self.next.setGeometry(QtCore.QRect(450, 510, 130, 40))
+        self.next.setStyleSheet("background-color:\"Dodgerblue\"; color:\"white\";font: 16pt\"경기천년제목M Medium\";")
+        self.next.setObjectName("next")
+        self.logo = QtWidgets.QWidget(HowtoUse_step1)
+        self.logo.setGeometry(QtCore.QRect(730, 20, 90, 60))
+        self.logo.setStyleSheet("background-color:\"Dodgerblue\";")
+        self.logo.setObjectName("logo")
+        self.home_button = QtWidgets.QWidget(HowtoUse_step1)
+        self.home_button.setGeometry(QtCore.QRect(30, 20, 60, 60))
+        self.home_button.setStyleSheet("background-color:\"Dodgerblue\";")
+        self.home_button.setObjectName("home_button")
+        icon.addPixmap(QtGui.QPixmap(":/icon_heart/heart.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
 
-        self.retranslateUi(selectExercise)
-        QtCore.QMetaObject.connectSlotsByName(selectExercise)
+        self.retranslateUi(HowtoUse_step1)
+        QtCore.QMetaObject.connectSlotsByName(HowtoUse_step1)
 
-    def retranslateUi(self, selectExercise):
+    def retranslateUi(self, HowtoUse_step1):
         _translate = QtCore.QCoreApplication.translate
-        selectExercise.setWindowTitle(_translate("selectExercise", "HomePT와 함께 하는 올바른 홈트레이닝 라이프"))
-        self.selectTitle.setText(_translate("selectExercise", "<html><head/><body><p align=\"center\">Step1. 원하는 운동을 선택해주세요</p><p align=\"center\"><br/></p></body></html>"))
-        self.exButton2.setText(_translate("selectExercise", "next"))
+        HowtoUse_step1.setWindowTitle(_translate("HowtoUse_step1", "HomePT와 함께 하는 올바른 홈트레이닝 라이프"))
+        self.selectTitle.setText(_translate("HowtoUse_step1", "<html><head/><body><p align=\"center\">Step1. 원하는 운동을 선택해주세요</p><p align=\"center\"><br/></p></body></html>"))
+        self.next.setText(_translate("HowtoUse_step1", "next"))
 
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    selectExercise = QtWidgets.QDialog()
-    ui = Ui_selectExercise()
-    ui.setupUi(selectExercise)
-    selectExercise.show()
+    HowtoUse_step1 = QtWidgets.QDialog()
+    ui = Ui_HowtoUse_step1()
+    ui.setupUi(HowtoUse_step1)
+    HowtoUse_step1.show()
     sys.exit(app.exec_())
 
--- 3355/HowtoUse_step3.py
+++ 3355/HowtoUse_step3.py
@@ -8,60 +8,64 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 
-class Ui_selectExercise(object):
-    def setupUi(self, selectExercise):
-        selectExercise.setObjectName("selectExercise")
-        selectExercise.resize(850, 600)
+class Ui_HowtoUse_step3(object):
+    def setupUi(self, HowtoUse_step3):
+        HowtoUse_step3.setObjectName("selectExercise")
+        HowtoUse_step3.resize(850, 600)
         icon = QtGui.QIcon()
         icon.addPixmap(QtGui.QPixmap(":/icon_heart/heart.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
-        selectExercise.setWindowIcon(icon)
-        selectExercise.setStyleSheet("background-color:\"Aliceblue\";")
-        self.selectTitle = QtWidgets.QLabel(selectExercise)
-        self.selectTitle.setGeometry(QtCore.QRect(100, 440, 650, 61))
+        HowtoUse_step3.setWindowIcon(icon)
+        HowtoUse_step3.setStyleSheet("background-color:\"Aliceblue\";")
+        self.selectTitle = QtWidgets.QLabel(HowtoUse_step3)
+        self.selectTitle.setGeometry(QtCore.QRect(100, 440, 650, 60))
         self.selectTitle.setStyleSheet("font: 20pt\"경기천년제목M Medium\"; color:\"black\";")
         self.selectTitle.setObjectName("selectTitle")
-        self.Photo = QtWidgets.QWidget(selectExercise)
-        self.Photo.setGeometry(QtCore.QRect(210, 120, 431, 301))
+        self.Photo = QtWidgets.QWidget(HowtoUse_step3)
+        self.Photo.setGeometry(QtCore.QRect(210, 120, 430, 300))
         self.Photo.setAutoFillBackground(False)
         self.Photo.setStyleSheet("background-color:\"Dodgerblue\";image: url(:/exercise1/ex1.png);")
         self.Photo.setObjectName("Photo")
-        self.exButton1 = QtWidgets.QPushButton(selectExercise)
-        self.exButton1.setGeometry(QtCore.QRect(260, 510, 141, 40))
-        self.exButton1.setStyleSheet("background-color:\"Dodgerblue\"; color:\"white\";font: 16pt\"경기천년제목M Medium\";")
-        self.exButton1.setObjectName("exButton1")
-        self.exButton2 = QtWidgets.QPushButton(selectExercise)
-        self.exButton2.setGeometry(QtCore.QRect(450, 510, 131, 40))
-        self.exButton2.setStyleSheet("background-color:\"Dodgerblue\"; color:\"white\";font: 16pt\"경기천년제목M Medium\";")
-        self.exButton2.setObjectName("exButton2")
-        self.widget = QtWidgets.QWidget(selectExercise)
-        self.widget.setGeometry(QtCore.QRect(730, 20, 91, 61))
-        self.widget.setStyleSheet("background-color:\"Dodgerblue\";")
-        self.widget.setObjectName("widget")
-        self.widget_2 = QtWidgets.QWidget(selectExercise)
-        self.widget_2.setGeometry(QtCore.QRect(30, 20, 91, 61))
-        self.widget_2.setStyleSheet("background-color:\"Dodgerblue\";")
-        self.widget_2.setObjectName("widget_2")
+        self.prev = QtWidgets.QPushButton(HowtoUse_step3)
+        self.prev.setGeometry(QtCore.QRect(260, 510, 140, 40))
+        self.prev.setStyleSheet("background-color:\"red\"; color:\"white\";font: 16pt\"경기천년제목M Medium\";")
+        self.prev.setObjectName("prev")
+        self.start = QtWidgets.QPushButton(HowtoUse_step3)
+        self.start.setGeometry(QtCore.QRect(450, 510, 130, 40))
+        self.start.setStyleSheet("background-color:\"Dodgerblue\"; color:\"white\";font: 16pt\"경기천년제목M Medium\";")
+        self.start.setObjectName("start")
+        self.logo = QtWidgets.QWidget(HowtoUse_step3)
+        self.logo.setGeometry(QtCore.QRect(730, 20, 90, 60))
+        self.logo.setStyleSheet("background-color:\"Dodgerblue\";")
+        self.logo.setObjectName("logo")
+        self.home_button = QtWidgets.QWidget(HowtoUse_step3)
+        self.home_button.setGeometry(QtCore.QRect(30, 20, 60, 60))
+        self.home_button.setStyleSheet("background-color:\"Dodgerblue\";")
+        self.home_button.setObjectName("home_button")
 
-        self.retranslateUi(selectExercise)
-        QtCore.QMetaObject.connectSlotsByName(selectExercise)
+        self.retranslateUi(HowtoUse_step3)
+        QtCore.QMetaObject.connectSlotsByName(HowtoUse_step3)
 
-    def retranslateUi(self, selectExercise):
+        #self.startButton.clicked.connect(self.start_clicked)  # start_clicked 함수 호출
+
+    # start_clicked 함수 선언 - 메인 페이지에서 운동 선택 페이지로 전환
+    #def start_clicked(self):
+    #    homePT.close()
+    #    selectEx.show()
+
+    def retranslateUi(self, HowtoUse_step3):
         _translate = QtCore.QCoreApplication.translate
-        selectExercise.setWindowTitle(_translate("selectExercise", "HomePT와 함께 하는 올바른 홈트레이닝 라이프"))
-        self.selectTitle.setText(_translate("selectExercise", "<html><head/><body><p align=\"center\">Step3. 결과 확인</p></body></html>"))
-        self.exButton1.setText(_translate("selectExercise", "prev"))
-        self.exButton2.setText(_translate("selectExercise", "start"))
+        HowtoUse_step3.setWindowTitle(_translate("HowtoUse_step3", "HomePT와 함께 하는 올바른 홈트레이닝 라이프"))
+        self.selectTitle.setText(_translate("HowtoUse_step3", "<html><head/><body><p align=\"center\">Step3. 결과 확인</p></body></html>"))
+        self.prev.setText(_translate("HowtoUse_step3", "prev"))
+        self.start.setText(_translate("HowtoUse_step3", "start"))
 
-import e1_rc
-import e2_rc
-import heart_rc
+
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
-    selectExercise = QtWidgets.QDialog()
-    ui = Ui_selectExercise()
-    ui.setupUi(selectExercise)
-    selectExercise.show()
+    HowtoUse_step3 = QtWidgets.QDialog()
+    ui = Ui_HowtoUse_step3()
+    ui.setupUi(HowtoUse_step3)
+    HowtoUse_step3.show()
     sys.exit(app.exec_())
-
