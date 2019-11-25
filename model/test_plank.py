9182baa120e2d790bc506d03d91cc3918b0143f2
jrucl2d <53755037+jrucl2d@users.noreply.github.com>
2019-11-25 13:38:01 +0900

    Update test_plank.py

--- model/test_plank.py
+++ model/test_plank.py
@@ -8,6 +8,27 @@ import matplotlib.pyplot as plt
 from matplotlib import collections  as mc
 import time, math
 import PIL.ExifTags as ExifTags
+import cv2
+
+# get video and slice into frame
+vidcap = cv2.VideoCapture('./stacked_hourglass/datasets/video/z.mp4')
+img_loc = " "
+def getFrame(sec):
+    vidcap.set(cv2.CAP_PROP_POS_MSEC,sec*1000)
+    hasFrames,image = vidcap.read()
+    if hasFrames:
+        img_loc = "image"+str(count)+".jpg"
+        cv2.imwrite(img_loc, image)     # save frame as JPG file
+    return hasFrames
+sec = 0
+frameRate = 0.1 #//it will capture image in each 0.5 second
+count = 100
+success = getFrame(sec)
+while success:
+    count = count + 1
+    sec = sec + frameRate
+    sec = round(sec, 2)
+    success = getFrame(sec)
 
 # ...load image of a person into a PyTorch tensor...
 
@@ -19,7 +40,7 @@ print("==model loaded==")
 RGB_MEAN = torch.as_tensor([0.4404, 0.4440, 0.4327])
 RGB_STDDEV = torch.as_tensor([0.2458, 0.2410, 0.2468])
 #im = np.asarray(Image.open("./images/test14]].jpeg"))
-orgImg = Image.open("./stacked_hourglass/datasets/up/f/3.jpg")
+orgImg = Image.open("./image100.jpg")
 try:
     for orientation in ExifTags.TAGS.keys() :
             if ExifTags.TAGS[orientation]=='Orientation' : break
@@ -50,21 +71,21 @@ print("infer time : ",end-start)
 
 fig, ax = plt.subplots()
 plt.imshow(im)
-c = np.array([(0, 1, 1, 0.5),
-              (0, 1, 1, 0.5),
+c = np.array([(0, 0, 1, 0.5),
               (0, 0, 1, 0.5),
               (0, 0, 1, 0.5),
-              (0, 0.3, 0.7, 0.5),
-              (0, 0.3, 0.7, 0.5),
-              (0.1, 0.6, 0.7, 0.5),
-              (0.1, 0, 0.2, 0.5),
-              (0, 0.5, 0.5, 0.5),
-              (0.1, 0.6, 0.2, 0.5),
-              (0, 1, 0.15, 0.5),
-              (0, 1, 0.15, 0.5),
               (0, 0, 1, 0.5),
               (0, 0, 1, 0.5),
-              (0, 0.3, 0.5, 0.5)])
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5),
+              (0, 0, 1, 0.5)])
 lines = [[(ys[0], xs[0]), (ys[1], xs[1])],
          [(ys[1], xs[1]), (ys[2], xs[2])],
          [(ys[5], xs[5]), (ys[4], xs[4])],
@@ -81,10 +102,6 @@ lines = [[(ys[0], xs[0]), (ys[1], xs[1])],
          [(ys[14], xs[14]), (ys[13], xs[13])],
          [(ys[14], xs[14]), (ys[15], xs[15])]]
 
-lc = mc.LineCollection(lines, colors=c,linewidths=2)
-ax.add_collection(lc)
-plt.scatter(ys, xs)
-
 left_antebrachial   = np.array([ys[15]-ys[14],xs[15]-xs[14]])
 left_forearm        = np.array([ys[13]-ys[14],xs[13]-xs[14]])
 left_back           = np.array([ys[7]-ys[13], xs[7]-xs[13]])
@@ -98,18 +115,43 @@ right_arm_angle     = np.inner(right_antebrachial, right_forearm)/(np.linalg.nor
 right_back_angle    = np.inner(right_back, right_forearm)/(np.linalg.norm(right_back)*np.linalg.norm(right_forearm))
 
 head_neck           = np.array([ys[9]-ys[8],xs[9]-xs[8]])
-neck_chest          = np.array([ys[7]-ys[8],xs[7]-xs[8]])
-chest_spine         = np.array([ys[6]-ys[7], xs[6]-xs[7]])
-neck_angle          = np.inner(head_neck, neck_chest)/(np.linalg.norm(head_neck)*np.linalg.norm(neck_chest))
-chest_angle         = np.inner(neck_chest, chest_spine)/(np.linalg.norm(neck_chest)*np.linalg.norm(chest_spine))
+chest_neck          = np.array([ys[7]-ys[8],xs[7]-xs[8]])
 
-hip                 = np.array([ys[6]-ys[7],xs[6]-xs[7]])
-leg                 = np.array([ys[4]-ys[3],xs[4]-xs[3]])
-hip_angle           = np.inner(hip, leg)/(np.linalg.norm(hip)*np.linalg.norm(leg))
+neck_chest          = np.array([ys[8]-ys[7],xs[8]-xs[7]])
+hip_chest           = np.array([ys[6]-ys[7],xs[6]-xs[7]])
+
+chest_hip           = np.array([ys[7]-ys[6],xs[7]-xs[6]])
+knee_plevis         = np.array([ys[4]-ys[3],xs[4]-xs[3]])
+
+neck_angle          = np.inner(head_neck, chest_neck)/(np.linalg.norm(head_neck)*np.linalg.norm(chest_neck))
+chest_angle         = np.inner(neck_chest, hip_chest)/(np.linalg.norm(neck_chest)*np.linalg.norm(hip_chest))
+hip_angle           = np.inner(chest_hip, knee_plevis)/(np.linalg.norm(chest_hip)*np.linalg.norm(knee_plevis))
 
 print("목 각    : ",np.arccos(neck_angle)*360/(np.pi*2))
-print("명치 각   : ",180-np.arccos(chest_angle)*360/(np.pi*2))
-print("빵댕이 각    : ",180-np.arccos(hip_angle)*360/(np.pi*2))
+print("가슴 각   : ",np.arccos(chest_angle)*360/(np.pi*2))
+print("엉덩이 각    : ",np.arccos(hip_angle)*360/(np.pi*2))
+
+neck  = np.arccos(neck_angle)*360/(np.pi*2)
+chest = np.arccos(chest_angle)*360/(np.pi*2)
+hip   = np.arccos(hip_angle)*360/(np.pi*2)
+
+if(170 > neck or neck >= 180):   # 목 각도가 오차 범위를 벗어난 경우
+    c[8] = (1, 0, 0, 0.5)  # line color = red
+    c[7] = (1, 0, 0, 0.5)  # line color = red
+if(165 > chest or chest >= 180):  # 가슴 각도가 오차 범위를 벗어난 경우
+    c[7] = (1, 0, 0, 0.5)  # line color = red
+    c[6] = (1, 0, 0, 0.5)  # line color = red
+if(165 > hip or hip >= 180):    # 엉덩이 각도가 오차 범위를 벗어난 경우
+    c[6] = (1, 0, 0, 0.5)  # line color = red
+    c[5] = (1, 0, 0, 0.5)  # line color = red
+    c[4] = (1, 0, 0, 0.5)  # line color = red
+    c[3] = (1, 0, 0, 0.5)  # line color = red
+
+    c[1] = (1, 0, 0, 0.5)  # line color = red
+
+lc = mc.LineCollection(lines, colors=c,linewidths=2)
+ax.add_collection(lc)
+plt.scatter(ys, xs)
 
 #print("왼쪽 팔 각    : ",np.arccos(left_arm_angle)*360/(np.pi*2))
 #print("왼쪽 어깨 각   : ",180-np.arccos(left_back_angle)*360/(np.pi*2))
@@ -119,5 +161,7 @@ print("빵댕이 각    : ",180-np.arccos(hip_angle)*360/(np.pi*2))
 
 # 플랭크를 위한 다리 각도 계산
 
-plt.show()
+plt.show() # keypoint detection 이미지로 출력
+
+
 
