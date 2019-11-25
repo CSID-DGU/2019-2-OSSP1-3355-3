1b0694a80dfe2c413c6c975f27cc2aea13a70c98
ym-o <55729131+ym-o@users.noreply.github.com>
2019-11-25 13:50:32 +0900

    Add files via upload

--- 3355/realMain.py
+++ 3355/realMain.py
@@ -1,6 +1,10 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import *
+from videoToframe import *
+
+
+global fname
 
 # 메인 페이지
 class Ui_homePT(object):
@@ -333,9 +337,11 @@ class SelectFile(QWidget):
 
     # pushButtonClicked 함수 선언 - 파일 불러오기 창 띄우기
     def pushButtonClicked(self):
+        global fname
         fname = QFileDialog.getOpenFileName(self, 'Open file', "", "Video File(*.avi, *.mp4)")
         if fname[0]:            # videoToframe.py로 변수 전달
             print(fname[0])
+
         else:
             QMessageBox.about(self, "Warning", "파일을 선택하지 않았습니다.")
 
